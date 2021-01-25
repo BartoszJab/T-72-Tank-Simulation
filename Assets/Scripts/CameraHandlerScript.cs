8b6851c2d31dd8e36463efa76630ce6fd90c2ece
Bartosz <bartekj1221@wp.pl>
2021-01-25 11:27:12 +0100

    First commit

---
+++ Assets/Scripts/CameraHandlerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandlerScript : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera rotateModeCamera;
+    
+    [HideInInspector]
+    public bool isRotateMode = false;
+
+    void Start()
+    {
+        mainCamera.enabled = true;
+        rotateModeCamera.enabled = false;
+    }
+
+    void Update()
+    {
+        // switch between main 'game mode' camera and 'rotate mode' camera
+        if (Input.GetKeyDown(KeyCode.LeftAlt)) {
+            isRotateMode = !isRotateMode;
+
+            ChangeCameras();
+
+            // set second camera position to the position of the main camera
+            rotateModeCamera.transform.SetPositionAndRotation(mainCamera.transform.position, mainCamera.transform.rotation);
+        }
+    }
+
+    private void ChangeCameras() {
+        mainCamera.enabled = !mainCamera.enabled;
+        rotateModeCamera.enabled = !rotateModeCamera.enabled;
+    }
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    [SerializeField] private GameObject _camerasHandler;
+    private CameraHandlerScript cameraHandlerScript;
+
+    public GameObject capsule;
+    public float power = 5.0f;
+    public float capsuleRotationSpeed = 45f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        cameraHandlerScript = _camerasHandler.GetComponent<CameraHandlerScript>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        Move();
+
+        if (!cameraHandlerScript.isRotateMode) {
+            RotateCapsule();
+        }
+        
+    }
+
+    private void RotateCapsule() {
+        capsule.transform.Rotate(0f, (Input.GetAxis("Mouse X") * capsuleRotationSpeed * Time.deltaTime), 0f);
+    }
+
+    // temporary template movement function
+    private void Move() {
+        float moveForward = Input.GetAxis("Vertical") * power;
+
+        rb.AddForce(Vector3.forward * moveForward);
+    }
+}
