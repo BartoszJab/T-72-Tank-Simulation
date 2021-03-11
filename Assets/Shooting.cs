79c3307a847985b9c8c41ce83bce4399b43a94a4
Bartosz <bartekj1221@wp.pl>
2021-03-11 19:10:13 +0100

    spawning projectile and adding force to it

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public Transform bulletSpawner;
+    public Rigidbody projectilePrefab;
+    public float shootingPower = 100_000f;
+
+    private bool isShooting = false;
 
     void Start()
     {
-        
+
     }
 
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("mouse btn pressed");
+            isShooting = true;
+        }
+
     }
+
+    private void FixedUpdate() {
+        Shoot();
+    }
+
+    private void Shoot() {
+        if (isShooting) {
+            Vector3 position = bulletSpawner.position;
+            Rigidbody projectileRb = Instantiate(projectilePrefab, position, bulletSpawner.rotation) as Rigidbody;
+            projectileRb.AddRelativeForce(new Vector3(0, 0, shootingPower));
+            
+            isShooting = false;
+        }
+    }
+
+
 }
