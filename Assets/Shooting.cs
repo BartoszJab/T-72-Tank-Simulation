92c36c80acf625b45e63665b4b16d592e06bcf33
Bartosz <bartekj1221@wp.pl>
2021-03-11 17:23:22 +0100

    shooting init

---
+++ Assets/Shooting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform bulletSpawner;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
