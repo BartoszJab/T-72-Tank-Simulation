98c6d3903a0236e485aa64894aa9bce3805d3f01
Bartosz <bartekj1221@wp.pl>
2021-04-18 13:33:30 +0200

    handling aiming camera

--- Assets/Scripts/CameraHandlerScript.cs
+++ Assets/Scripts/CameraHandlerScript.cs
@@ -6,15 +6,42 @@ public class CameraHandlerScript : MonoBehaviour
     
     [HideInInspector]
     public static bool isRotateMode = false;
+    public static bool isMainMode = true;
+    public static bool isAmingMode = false;
+
+    public Camera aimCamera;
+
+    private Vector3 prevMainCameraPosition;
 
     private void Start() {
         mainCamera = Camera.main;
+        aimCamera.enabled = false;
+
+        // hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftAlt)) {
+            if (isAmingMode) return;
+            isAmingMode = false;
             isRotateMode = !isRotateMode;
         }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1)) {
+            if (isRotateMode) return;
+            isAmingMode = !isAmingMode;
+
+            if (isAmingMode) {
+                mainCamera.enabled = false;
+                aimCamera.enabled = true;
+            } else {
+                mainCamera.enabled = true;
+                aimCamera.enabled = false;
+            }
+        }
+
     }
 }
