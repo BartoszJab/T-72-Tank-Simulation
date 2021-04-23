8b9db839d6be393c4fb79bff7ceb518f1a750072
Bartosz <bartekj1221@wp.pl>
2021-04-23 19:09:23 +0200

    added bunker object, fractured bunker object and script responsible for replacing those objects for destruction simulation

---
+++ Assets/Destructable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject fracturedBunker;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Projectile") {
+            Instantiate(fracturedBunker, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
