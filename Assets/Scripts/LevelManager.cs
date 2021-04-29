ded3efc13ee0b0974c792053b05412dfb0364bcc
Bartosz <bartekj1221@wp.pl>
2021-04-29 19:10:36 +0200

    level manager script that checks if the game's objective was accomplished

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject bunkersHolder;
+    public TMP_Text objectiveText;
+    public static int bunkersCount;
+
+    public GameObject EndGameCanvas;
+    public TMP_Text completionText;
+    private static float completionTime = 0f;
+    private float timeToLoadMenu = 5f;
+
+    void Awake()
+    {
+        bunkersCount = bunkersHolder.transform.childCount;
+        objectiveText.text = "Buncers left to destroy: " + bunkersCount;
+        EndGameCanvas.SetActive(false);
+    }
+
+    private void Update() {
+        objectiveText.text = "Buncers left to destroy: " + bunkersCount;
+
+        if (bunkersCount > 0) {
+            completionTime += Time.deltaTime;
+        } else {
+            EndGameCanvas.SetActive(true);
+            completionText.text = "It took you " + Mathf.Round(completionTime) + " seconds!";
+
+            if (timeToLoadMenu >= 0) {
+                timeToLoadMenu -= Time.deltaTime;
+            } else {
+                SceneManager.LoadScene(Level.MainMenu);
+            }
+        }
+
+    }
+
+
+}
